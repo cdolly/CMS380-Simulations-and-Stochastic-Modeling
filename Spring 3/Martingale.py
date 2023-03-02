55be589d8e98cd905b1de61f7d04116864ffe402
Cameron Dolly <72312670+cdolly@users.noreply.github.com>
2023-03-02 11:19:33 -0500

    Update Martingale.py

--- Spring 3/Martingale.py
+++ Spring 3/Martingale.py
@@ -1,34 +1,53 @@
+"""
+Martingale Problem
+
+"""
 import random
+import matplotlib.pyplot as plt
 
-def play_martingale(starting_bet, starting_bankroll, max_spins, max_loss_streak):
-    bankroll = starting_bankroll
-    bet = starting_bet
-    spins = 0
-    loss_streak = 0
-    while spins < max_spins and bet <= bankroll:
-        result = random.choice(['red', 'black'])
-        if result == 'black':
-            bankroll += bet
-            bet = starting_bet
-            loss_streak = 0
-        else:
-            bankroll -= bet
-            bet *= 2
-            loss_streak += 1
-            if loss_streak >= max_loss_streak:
-                break
-        spins += 1
-    return bankroll
 
-starting_bet = 1
-starting_bankroll = 255
-max_spins = 100
-max_loss_streak = 8
+def simulate(startingBet, startingBankroll, maxSpins, maxLossStreak):
+  bankroll = startingBankroll
+  bet = startingBet
+  spins = 0
+  lossStreak = 0
+  netgain = 0
 
-results = []
+  while spins < maxSpins and bet <= bankroll:
+    result = random.choice(['red', 'black'])
+
+    if result == 'black':
+      bankroll += bet
+      netgain += bet
+      bet = startingBet
+      lossStreak = 0
+
+    else:
+      bankroll -= bet
+      netgain -= bet
+      bet *= 2
+      lossStreak += 1
+      if lossStreak >= maxLossStreak:
+        break
+    spins += 1
+
+  return netgain
+
+
+startingBet = 1
+startingBankroll = 255
+maxSpins = 100
+maxLossStreak = 8
+
+gameResults = []
 for i in range(1000):
-    final_bankroll = play_martingale(starting_bet, starting_bankroll, max_spins, max_loss_streak)
-    results.append(final_bankroll)
+  finalBankroll = simulate(startingBet, startingBankroll, maxSpins,
+                           maxLossStreak)
+  gameResults.append(finalBankroll)
 
-print(f"Average final bankroll: ${sum(results) / len(results):,.2f}")
-print(f"Number of bankruptcies: {results.count(0)}")
+plt.figure()
+plt.hist(gameResults, 20)
+plt.title("Histogram of Game Results")
+plt.xlabel("Net Gain of Bankroll")
+plt.ylabel("Count")
+plt.savefig('histogram.pdf', bbox_inches='tight')
